d8cd62f69a01470297d548984dda364843b68171
muhwezideo <aggrey256@gmail.com>
2018-12-27 19:24:24 +0300

    restructure project

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,38 +1,17 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
 import './Blog.css';
-import axios from 'axios';
-
+// import axios from 'axios';
+import Posts from './Posts/Posts';
 class Blog extends Component {
   state={
     posts:[],
     selectedPostId:null
   }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(response=>{
-      const posts= response.data.slice(0,4)
-      const updatedPosts=posts.map(post=>{
-        return {
-          ...post,
-          author:'Max'
-        }
-      });
-      this.setState({posts:updatedPosts})
-      // console.log(response.data)
-    });
-  }
-  postSelectedHandler=(id)=>{
-    this.setState({selectedPostId:id})
-  }
-
+  
+ 
     render () {
-        const posts =this.state.posts.map(post=>{
-
-          return <Post title={post.title} key={post.id } author={post.author}
-            clicked={()=>this.postSelectedHandler(post.id)}/>
-        })
+      
         return (
             <div className="Blog">
               <header>
@@ -43,15 +22,14 @@ class Blog extends Component {
                   </ul>
                 </nav>
               </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+            
                 {/* <section>
                     <FullPost  id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
                 </section> */}
+                <Posts/>
             </div>
         );
     }
