3de7a2245583c33e8a1da0bfd8ed40bb7bd8cb2c
muhwezideo <aggrey256@gmail.com>
2018-12-27 20:46:56 +0300

    with router

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -12,3 +12,22 @@ const post = (props) => (
 );
 
 export default post;
+
+
+// import React from 'react';
+// import {withRouter} from 'react-router-dom';
+// import './Post.css';
+
+// const post = (props) => {
+//     console.log(props.history)
+//     return(
+//     <article className="Post" onClick={props.clicked}>
+//         <h1>{props.title}</h1>
+//         <div className="Info">
+//             <div className="Author">{props.author}</div>
+//         </div>
+//     </article>
+//     )
+// };
+
+// export default withRouter(post);
