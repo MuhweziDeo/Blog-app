39a2783f6f5c447acb7a2a60cfe179ce35a64e15
muhwezideo <aggrey256@gmail.com>
2018-12-28 19:46:29 +0300

    add link to post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import './Blog.css';
 import Posts from './Posts/Posts';
 import {Route,NavLink} from 'react-router-dom';
 import NewPost from '../Blog/NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
   state={
@@ -41,6 +42,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={()=><Posts/>}/> */}
                 <Route path='' exact component={Posts}/>
                 <Route path='/new_post' exact component={NewPost}/>
+                <Route path="/:id" component={FullPost}/>
                 {/* <Route path="/new_posts" exact render={()=><h1>Posts</h1>}/> */}
             </div>
         );
