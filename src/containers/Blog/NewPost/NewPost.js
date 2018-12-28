325ffd143bfca56aa97f7f15010a0e2c6330737c
muhwezideo <aggrey256@gmail.com>
2018-12-28 21:14:46 +0300

    conditional redirect

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import {Redirect} from 'react-router-dom'
 import './NewPost.css';
 import axios from 'axios';
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted:false
     }
     postDataHandler=()=>{
       const data={
@@ -16,11 +17,18 @@ class NewPost extends Component {
       }
       axios.post('https://jsonplaceholder.typicode.com/posts/',data).then(response=>{
         console.log(response)
+        this.setState({submitted:true})
       })
     }
     render () {
+        let redirect=null;
+        if(this.state.submitted){
+            redirect=   <Redirect to="posts"/>
+
+        }
         return (
             <div className="NewPost">
+            {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
