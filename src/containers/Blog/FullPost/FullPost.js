9beeb27a847469b32122bc80da6c284eea09dc04
muhwezideo <aggrey256@gmail.com>
2018-12-28 21:00:55 +0300

    create dynamic routes

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,8 +10,15 @@ class FullPost extends Component {
 
     componentDidMount () {
         console.log(this.props.match.params.id)
+        this.loadData();
+     
+    }
+    componentDidUpdate(){
+        this.loadData();
+    }
+    loadData(){
         if ( this.props.match.params.id) {
-            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id != this.props.match.params.id) ) {
                 axios.get( 'https://jsonplaceholder.typicode.com/posts/' + this.props.match.params.id  )
                     .then( response => {
                         // console.log(response);
@@ -23,7 +30,7 @@ class FullPost extends Component {
         }
     }
     deletePostHandler=()=>{
-      axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id ).then(
+      axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.match.params.id ).then(
         response=>{
           console.log(response);
         }
@@ -31,7 +38,7 @@ class FullPost extends Component {
     }
     render () {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-        if ( this.props.id ) {
+        if ( this.props.match.params.id ) {
             post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
         }
         if ( this.state.loadedPost ) {
